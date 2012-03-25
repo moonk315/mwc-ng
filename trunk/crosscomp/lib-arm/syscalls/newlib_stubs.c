5efca8d2baacdaf667c883a7fa6560a668e05588
d.kayukov@gmail.com <d.kayukov@gmail.com@6c0838ad-2799-768d-6938-ab8755fc942b>
2012-03-25 05:38:31 +0000

    Using libc-printf() for now.

--- trunk/crosscomp/lib-arm/syscalls/newlib_stubs.c
+++ trunk/crosscomp/lib-arm/syscalls/newlib_stubs.c
@@ -192,6 +192,7 @@ int _read(int file, char *ptr, int len) {
     return num;
 }
 
+
 /*
  stat
  Status of a file (by name). Minimal implementation:
@@ -230,47 +231,3 @@ int _wait(int *status) {
     return -1;
 }
 
-/*
- write
- Write a character to a file. `libc' subroutines will use this system routine for output to all files, including stdout
- Returns -1 on error or number of bytes sent
- */
-int _write(int file, char *ptr, int len) {
-    int n;
-    switch (file) {
-    case STDOUT_FILENO: /*stdout*/
-        for (n = 0; n < len; n++) {
-#if STDOUT_USART == 1
-            while ((USART1->SR & USART_FLAG_TC) == (uint16_t)RESET) {}
-            USART1->DR = (*ptr++ & (uint16_t)0x01FF);
-#elif  STDOUT_USART == 2
-            while ((USART2->SR & USART_FLAG_TC) == (uint16_t) RESET) {
-            }
-            USART2->DR = (*ptr++ & (uint16_t) 0x01FF);
-#elif  STDOUT_USART == 3
-            while ((USART3->SR & USART_FLAG_TC) == (uint16_t)RESET) {}
-            USART3->DR = (*ptr++ & (uint16_t)0x01FF);
-#endif
-        }
-        break;
-    case STDERR_FILENO: /* stderr */
-        for (n = 0; n < len; n++) {
-#if STDERR_USART == 1
-            while ((USART1->SR & USART_FLAG_TC) == (uint16_t)RESET) {}
-            USART1->DR = (*ptr++ & (uint16_t)0x01FF);
-#elif  STDERR_USART == 2
-            while ((USART2->SR & USART_FLAG_TC) == (uint16_t) RESET) {
-            }
-            USART2->DR = (*ptr++ & (uint16_t) 0x01FF);
-#elif  STDERR_USART == 3
-            while ((USART3->SR & USART_FLAG_TC) == (uint16_t)RESET) {}
-            USART3->DR = (*ptr++ & (uint16_t)0x01FF);
-#endif
-        }
-        break;
-    default:
-        errno = EBADF;
-        return -1;
-    }
-    return len;
-}
