47d8711ac4e6c2a8fd27223d54eb61114c06a0db
d.kayukov@gmail.com <d.kayukov@gmail.com@6c0838ad-2799-768d-6938-ab8755fc942b>
2012-10-28 01:27:28 +0000

    *:math
    Minor cosmetic changes.

--- trunk/crosscomp/src/MWC_Math.h
+++ trunk/crosscomp/src/MWC_Math.h
@@ -11,7 +11,7 @@ float InvSqrt (float x){
 
 int32_t isq(int32_t x){return x * x;}
 
-int16_t _atan2(float y, float x){
+float _atan2(float y, float x){
   #define fp_is_neg(val) ((((uint8_t*)&val)[3] & 0x80) != 0)
   float z = y / x;
   int16_t zi = abs(int16_t(z * 100));
@@ -27,7 +27,9 @@ int16_t _atan2(float y, float x){
    z = (PI / 2.0f) - z / (z * z + 0.28f);
    if (y_neg) z -= PI;
   }
-  z *= (180.0f / PI * 100);
   return z;
 }
 
+inline float v3_norm(float x, float y, float z) {
+  return InvSqrt(x*x + y*y + z*z);
+}
