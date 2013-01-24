2ad53530fbbd8090f470a207435fcfde8173cfc3
d.kayukov@gmail.com <d.kayukov@gmail.com@6c0838ad-2799-768d-6938-ab8755fc942b>
2013-01-24 09:42:19 +0000

    mwc-ng: HAL
    Initial support of the stm32fdiscovery. Cosmetic changes.

--- trunk/crosscomp/src/conf/stm32fdisc_conf.h
+++ trunk/crosscomp/src/conf/stm32fdisc_conf.h
@@ -18,8 +18,8 @@
 #define BARO _NONE_
 
 #define ACC_ORIENTATION(X, Y, Z)  {imu.acc.fr.x  = -X; imu.acc.fr.y  = Y; imu.acc.fr.z  = Z;}
-#define GYRO_ORIENTATION(X, Y, Z) {imu.gyro_raw.eul.roll = -X; imu.gyro_raw.eul.pitch =  Y; imu.gyro_raw.eul.yaw = -Z;}
-#define MAG_ORIENTATION(X, Y, Z)  {imu.mag.fr.x  = X; imu.mag.fr.y  = -Z; imu.mag.fr.z  = -Y;}
+#define GYRO_ORIENTATION(X, Y, Z) {imu.gyro_raw.eul.roll = -Y; imu.gyro_raw.eul.pitch = X; imu.gyro_raw.eul.yaw = -Z;}
+#define MAG_ORIENTATION(X, Y, Z)  {imu.mag.fr.x  = X; imu.mag.fr.y  = -Y; imu.mag.fr.z  = -Z;}
 
 
 #endif // STM32FDISC_CONF_H_INCLUDED
