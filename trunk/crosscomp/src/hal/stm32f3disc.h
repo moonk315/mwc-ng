d2d338e71f3da67c2d6884fb59699d26b34b76a4
d.kayukov@gmail.com <d.kayukov@gmail.com@6c0838ad-2799-768d-6938-ab8755fc942b>
2013-01-25 02:51:47 +0000

    stm32f3disc:HAL
    - ADC

--- trunk/crosscomp/src/hal/stm32f3disc.h
+++ trunk/crosscomp/src/hal/stm32f3disc.h
@@ -203,6 +203,51 @@ inline void BeepToggle() {
 void Board_Idle() {
 };
 
+inline void init_adc() {
+  ADC_InitTypeDef       ADC_InitStructure;
+  ADC_CommonInitTypeDef ADC_CommonInitStructure;
+  /* Configure the ADC clock */
+  RCC_ADCCLKConfig(RCC_ADC12PLLCLK_Div2);
+  /* Enable ADC1 clock */
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_ADC12, ENABLE);
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
+  /* Configure ADC Channel7 as analog input */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 ;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
+  /* ADC Channel configuration */
+  ADC_StructInit(&ADC_InitStructure);
+  /* Calibration procedure */
+  ADC_VoltageRegulatorCmd(ADC1, ENABLE);
+  __delay_ms(10);
+  ADC_SelectCalibrationMode(ADC1, ADC_CalibrationMode_Single);
+  ADC_StartCalibration(ADC1);
+  while(ADC_GetCalibrationStatus(ADC1) != RESET );
+  //calibration_value = ADC_GetCalibrationValue(ADC1);
+  //
+  ADC_CommonInitStructure.ADC_Mode = ADC_Mode_Independent;
+  ADC_CommonInitStructure.ADC_Clock = ADC_Clock_AsynClkMode;
+  ADC_CommonInitStructure.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;
+  ADC_CommonInitStructure.ADC_DMAMode = ADC_DMAMode_OneShot;
+  ADC_CommonInitStructure.ADC_TwoSamplingDelay = 0;
+  ADC_CommonInit(ADC1, &ADC_CommonInitStructure);
+  //
+  ADC_InitStructure.ADC_ContinuousConvMode = ADC_ContinuousConvMode_Disable;
+  ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
+  ADC_InitStructure.ADC_ExternalTrigConvEvent = ADC_ExternalTrigConvEvent_0;
+  ADC_InitStructure.ADC_ExternalTrigEventEdge = ADC_ExternalTrigEventEdge_None;
+  ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+  ADC_InitStructure.ADC_OverrunMode = ADC_OverrunMode_Disable;
+  ADC_InitStructure.ADC_AutoInjMode = ADC_AutoInjec_Disable;
+  ADC_InitStructure.ADC_NbrOfRegChannel = 1;
+  ADC_Init(ADC1, &ADC_InitStructure);
+  ADC_RegularChannelConfig(ADC1, ADC_Channel_7, 1, ADC_SampleTime_7Cycles5);
+  ADC_Cmd(ADC1, ENABLE);
+  while(!ADC_GetFlagStatus(ADC1, ADC_FLAG_RDY));
+  ADC_StartConversion(ADC1);
+}
+
 inline void StartBatteryVoltageMeasurement() {
   ADC_StartConversion(ADC1);
 }
@@ -241,7 +286,6 @@ void USB_Cable_Config (uint8_t NewState) {
     GPIO_SetBits(USB_DISCONNECT, USB_DISCONNECT_PIN);
   }
 }
-
 
 void init_usb () {
  /* Enable the PWR clock */
@@ -295,6 +339,7 @@ inline void Board_Init() {
   init_spi();
   init_pwm(false);
   I2C_Init();
+  init_adc();
   RCC_ClearFlag();
 }
 
